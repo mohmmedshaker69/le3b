1cc21bda06563c5ddffbae6db418590caa3c107c
shakoor <mohmmedshaker69@gmail.com>
2024-03-21 14:38:06 +0200

    first commit

---
+++ le3b.py
@@ -0,0 +1,760 @@
+name=input("what is your name ")
+print("hi "+name)
+print("welocome to channel")
+nickname= input("what is your nickname ")
+about = input("what is your channel about ")
+print ("you could name your channel "+"(" + about  + " with " + nickname +"" )
+
+##################################################################
+
+print("welcome to your channel")
+nickname= input("write your nickname ")
+#=input("what is your channel about ")
+print("you could name your channel ") 
+  
+######################################################################
+
+print("shaker"[2])
+
+password=input("enter your password ")
+print(len  (password))
+
+############################################################
+
+num=input("inter two digit num ")
+print(int(num[0])+int(num[1]))
+
+#######################################################################
+
+width=int (input("enter the width " ))
+length=int (input("enter the length "))
+area= width*length
+money=area*100
+print("the area is equal to " , area )
+print("and the tech will take ", money , "from you")
+##############################################################################
+
+width=int(input("enter the width "))
+length=int(input("enter the length "))
+price= int(input("enter the price for one meter "))
+area=width*length
+offer=area*price
+print("area = " , area) 
+print("final price = " , offer)
+
+########################################################
+
+age=int( input("how old are you "))
+year=2023-age
+print("you was born in " , year)
+
+#########################################
+
+seconds=int(input("enter num of sec "))
+print(seconds//3600 ,"hours" ,(seconds%3600)//60 , "min" , ((seconds%3600)//60)%60,"sec")
+
+##################################################################################################
+
+age= int(input("what is your age "))
+if age >17:print("hello king")
+else :print("bye fucker")
+
+############################################################
+
+num=int(input("enter your number "))
+if num > 0 : print("your num is postive ")
+elif num ==0 :print("your num is eqyal to zero")
+else :print("your num is negative")
+
+############################################################
+
+password=int(input("enter you pass "))
+if password==123 : print("you are god damn right ") 
+else: print("wrong pass")  #########################################################
+
+word=input(" do you agree ")
+if word == "yes" :print ("you are good ")
+elif word == "no":print("you are hooker ")
+elif word == "maybe":print("you are human") 
+else : print("fuck")
+#################################
+
+age=int(input("what is your age "))
+lis=input("do you have a lisence (yes or no)") 
+if age>17 and lis.lower()=="yes": print("you can drive ") 
+else : print("you cant drive ")
+ 
+##################################
+
+nat=input("are you egyption ")
+if nat.lower() =="yes": age=(input("are you over 18 "))
+elif nat.lower()=="no" :print("you cant")
+if age.lower()=="yes" :print("you can have it")
+elif age.lower()=="no" :print ("you cant ")
+else:print("fuck")
+
+########################################################3
+
+egyp=input(" are you egyp ").lower()
+if egyp == "yes" :
+     print ("you can complete ")
+     age=input("are you over 18 ")
+     if age == "yes":
+          print("you can have it ")
+     else:print("you are under 18")
+else:print(" for egy")
+
+#########################################################
+
+print("""░░░░░░░▄▄████▄▄▄░░░░░░▄▄██████▄▄
+░░░░░██▓▓▓▓▓▓▒▓▓██░░▓█▓▓▓▓▒░▒▒▓▓██
+░░░██▓▓▓▓▓▓▓▓▓▒░▓▓███▓▓▓▓▓▒▒▒░░▒▓▓█
+░░██▓▓▓▓▓▓▓▓▓▓▓▒▒▓▓█▓▓▓▓▓▓▓▓▓▓▒░░▓▓█
+░▒█▓▓▓▓▓▓▓▓▓▓▓▓▓▓██▓██▓▓▓▓▓▓▓▓▓▒░░▓▓█
+░█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒░▒▓█
+░█▓▓▓▓▓▓▓▓▓▓▓▓████░████▓▓▓▓▓▓▓▓▓▒░░▓██
+█▓▓▓▓▓▓▓▓▓▓▓▓██░░░░░░░██▓▓▓▓▓▓▓▓▓░░▓▓█
+█▓▓▓▓▓▓▓▓▓▓▓▓▓░░█░░░█░░▓▓▓▓▓▓▓▓▓▓▒░▓▓█▒
+█▓▓▓▓▓▓▓▓▓▓▓▓▓░█▒█░█▒█░▓▓▓▓▓▓▓▓▓▓▒▒▓▓█▒
+█▓▓▓▓▓▓▓▓▓▓▓▓▓░░░░░░░░░▓▓▓▓▓▓▓▓▓▓▒▒▓██▒
+█▓▓▓▓▓▓▓▓▓▓▓▓▓▓░▒██░░░▓▓▓▓▓▓▓▓▓▓▓▒▓▓█▓▒
+░█▓▓▓▓▓▓▓▓▓▓▓▓▓▓████▓▓▓▓▓▓▓▓▓▓▓▓▓▒▓▓█▓▒
+░█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█▓▒
+░░█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█▓▒
+░░█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓███▓▓▓▓▓▓▓▓▓▓█▓▒
+░░░█▓▓▓▓▓▓▓▓▓██▓▓▓▓█████▓▓▓▓▓▓▓▓▓▓█▓▒
+░░░░█▓▓▓▓▓▓▓▓██████████▓▓▓▓▓▓▓▓▓▓██▓▒
+░░░░▓█▓▓▓▓▓▓▓▓██▒▒▓▒▒█▓▓▓▓▓▓▓▓▓▓██▓▒
+░░░░░░█▓▓▓▓▓▓▓▓██▒▒▒█▓▓▓▓▓▓▓▓▓▓██▓▒
+░░░░░░░██▓▓▓▓▓▓▓▓███▓▓▓▓▓▓▓▓▓▓██▓▒
+░░░░░░░░██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██▓▒
+░░░░░░░░░░█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█▓▒
+░░░░░░░░░░░██▓▓▓▓▓▓▓▓▓▓▓▓██▓▒
+░░░░░░░░░░░░░██▓▓▓▓▓▓▓▓▓█▓▒
+░░░░░░░░░░░░░░░██▓▓▓▓▓██▓▒
+░░░░░░░░░░░░░░░░░█▓▓██▓▒
+░░░░░░░░░░░░░░░░░░░█▓▒
+""") 
+print("welcome to mu island ")
+door=input("choose red or blue ").lower()
+if door== "red":
+    print ("nice choice ")
+    box=input(" white or black or green ").lower()
+    if box =="white":
+        print("you won ")
+    elif box == "black":
+        print("it is the snake you ara a dog ")
+    elif box== "green":
+        print ("it is pigs you are donkey ")
+    else:print("your answer is not included donkey ")
+else:print("you die ")
+
+########################################################################
+
+import random
+num=random.randint(-5,2)
+print(num)
+
+##########################################################
+
+import hello
+print(hello.ahmed)
+
+#####################################################
+
+import random
+passw=int( input("enter password "))
+num=random.randint(0000,9999)
+if len(str(passw)) !=4:
+    print("enter 4 nums")
+elif passw==num:
+    print("true")
+else :print("false",num)
+
+#################################################
+   
+import random
+coin=random.randint(1,2)
+toss=int(input("1 or 2 "))
+if toss==coin :
+    print("woooon ")
+else:print("you looooose")
+
+##################################################
+
+import random
+coose=input("random or randint ")
+if coose=="random" :
+    coin=random.random()
+    if coin>0.5 :
+        print ("head")
+    else:
+        print ("tail")
+elif coose=="randint":
+    coin=random.randint(0,1)
+    if coin==0:
+        print("tail")
+    else:
+        print("head")
+else:
+    print("plesae")    
+        
+##################################################
+
+print("hello" , "mohamed",sep=",,,,")
+
+###############################################
+
+x= float(input("enter x "))
+y= float(input("enter y "))
+z = round(x+y , 3)
+print(z,)
+
+############################################################
+
+import hello
+x=int(input("enter x "))
+print(hello. square(x))
+
+################################################
+
+i=3
+while i>0 : 
+    print("meow")
+    i=i-1
+
+
+
+for i in range (3,70):
+    print("a7a")
+
+#####################################
+
+print(""" meow """*5)
+
+#####################################
+
+while True:
+    x=int(input("enter x "))
+    if x <0:
+        continue
+    else : break
+
+for i in range(x):
+    print("meow")
+
+###############################################
+
+def shaker():
+    x=get_num()
+    meow(x)
+    
+   
+
+def meow(x):
+    for i in range(x):
+        print("meow")
+def get_num():
+    x=int(input("enter x "))
+    return x
+
+shaker ()  
+
+############################################################
+
+students=["shaker" , "fawzy" , "hesham"]
+for i in range (len(students)):
+    print(i+1,students[i])
+
+###############################################################
+
+students={"hany": "asyoot","shaker":"mansoura","ahmed":"damnhoor"}
+for student in (students):
+    print(student , students[student] , sep="=")
+
+###############################################################################
+
+students=[
+    
+       { "name":"hany", "city":"cairo","house":"2"},
+
+       { "name":"tex", "city":"cairo","house":"2"},
+
+         { "name":"matlzz", "city":"cairo","house":"2"}
+]
+
+for student in range(len(students))  :
+    print(student+1 ,students[student], sep="=")
+
+######################################################
+
+students=[
+    
+       { "name":"hany", "city":"cairo","house":"2"},
+
+       { "name":"tex", "city":"cairo","house":"2"},
+
+         { "name":"matlzz", "city":"cairo","house":"2"}
+]
+
+for student in students  :
+    print(student["name"])
+
+###############################################################
+
+def shaker():
+    n=getnum()
+    print_col(n)
+    
+
+
+def print_col(n):
+    for i in range(n):
+        print("#")
+    
+    
+def getnum():
+    n=int(input("enter num "))
+    return n
+
+shaker()
+    
+############################################################
+
+def shaker():
+    num=geth()
+    width=getw()
+    square(num,width)
+def geth():
+    num=int(input("enter height "))
+    return num
+
+def getw():
+    nums=int(input("enter width "))
+    return nums
+
+def square(num,nums):
+    for i in range (num):
+        print("#"*nums)  
+    
+
+
+
+shaker()      
+
+#############################################
+
+while True:
+    try:
+        x=int(input("what is x "))
+        
+    except ValueError:
+        print("x is not int ")
+    else:
+        break
+print("x is ", x)    
+
+###############################################################
+        
+
+def main ():
+    height=int(input("height : "))
+    prymids(height)
+
+def prymids(height):
+    for i in range(height):
+        print(i+1,"#"*(i+1))
+
+main()        
+
+
+###############################################################
+
+from random import randint
+
+coin=randint(1,2)
+x=input("head or tail ")
+if x==coin:
+    print("you won")
+else:
+    print("fuck you ")
+
+####################################################################
+
+import random 
+cards=["ahmed","amr","5ra"]
+random.shuffle(cards)
+for card in cards:
+    print(card)
+
+#####################################################################
+
+import statistics
+x=int(input("enter x "))
+y=int(input("enter y "))
+z=int(input("enter z "))
+means=statistics.mean([x,y,z])
+medians=statistics.median([x,y,z])
+print("your avg is ",means)
+print("your mid is ",medians)
+        
+####################################################
+
+import pyautogui
+import cowsay
+cowsay.trex("nada my sis ")
+
+print("""░░░░░░░▄▄████▄▄▄░░░░░░▄▄██████▄▄
+░░░░░██▓▓▓▓▓▓▒▓▓██░░▓█▓▓▓▓▒░▒▒▓▓██
+░░░██▓▓▓▓▓▓▓▓▓▒░▓▓███▓▓▓▓▓▒▒▒░░▒▓▓█
+░░██▓▓▓▓▓▓▓▓▓▓▓▒▒▓▓█▓▓▓▓▓▓▓▓▓▓▒░░▓▓█
+░▒█▓▓▓▓▓▓▓▓▓▓▓▓▓▓██▓██▓▓▓▓▓▓▓▓▓▒░░▓▓█
+░█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒░▒▓█
+░█▓▓▓▓▓▓▓▓▓▓▓▓████░████▓▓▓▓▓▓▓▓▓▒░░▓██
+█▓▓▓▓▓▓▓▓▓▓▓▓██░░░░░░░██▓▓▓▓▓▓▓▓▓░░▓▓█
+█▓▓▓▓▓▓▓▓▓▓▓▓▓░░█░░░█░░▓▓▓▓▓▓▓▓▓▓▒░▓▓█▒
+█▓▓▓▓▓▓▓▓▓▓▓▓▓░█▒█░█▒█░▓▓▓▓▓▓▓▓▓▓▒▒▓▓█▒
+█▓▓▓▓▓▓▓▓▓▓▓▓▓░░░░░░░░░▓▓▓▓▓▓▓▓▓▓▒▒▓██▒
+█▓▓▓▓▓▓▓▓▓▓▓▓▓▓░▒██░░░▓▓▓▓▓▓▓▓▓▓▓▒▓▓█▓▒
+░█▓▓▓▓▓▓▓▓▓▓▓▓▓▓████▓▓▓▓▓▓▓▓▓▓▓▓▓▒▓▓█▓▒
+░█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█▓▒
+░░█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█▓▒
+░░█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓███▓▓▓▓▓▓▓▓▓▓█▓▒
+░░░█▓▓▓▓▓▓▓▓▓██▓▓▓▓█████▓▓▓▓▓▓▓▓▓▓█▓▒
+░░░░█▓▓▓▓▓▓▓▓██████████▓▓▓▓▓▓▓▓▓▓██▓▒
+░░░░▓█▓▓▓▓▓▓▓▓██▒▒▓▒▒█▓▓▓▓▓▓▓▓▓▓██▓▒
+░░░░░░█▓▓▓▓▓▓▓▓██▒▒▒█▓▓▓▓▓▓▓▓▓▓██▓▒
+░░░░░░░██▓▓▓▓▓▓▓▓███▓▓▓▓▓▓▓▓▓▓██▓▒
+░░░░░░░░██▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓██▓▒
+░░░░░░░░░░█▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓█▓▒
+░░░░░░░░░░░██▓▓▓▓▓▓▓▓▓▓▓▓██▓▒
+░░░░░░░░░░░░░██▓▓▓▓▓▓▓▓▓█▓▒
+░░░░░░░░░░░░░░░██▓▓▓▓▓██▓▒
+░░░░░░░░░░░░░░░░░█▓▓██▓▒
+░░░░░░░░░░░░░░░░░░░█▓▒
+""")
+
+import json
+import requests
+import sys
+if len(sys.argv)!=2:
+    sys.exit()
+response=requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=",sys.argv[1])    
+print(json.dumps(response.json(),indent=2))
+ 
+       
+###############################################################
+
+def main():
+    n=int(input(" inter int "))
+    
+   
+    print(   square(n))
+
+
+def square(n):
+    num=n
+    
+    return num      
+
+
+
+if __name__=="__main__":
+    main()
+
+
+def test_1():
+    sqyre_test()
+
+def sqyre_test():
+    assert square(2)==4
+    assert square(4)==16
+test_1()
+
+
+from test import square_test
+
+
+
+
+
+
+
+
+
+
+
+
+def shaker():
+    square_test()    
+
+def square_test():
+    if square(2)!=4:
+        print ("there is a bug ")
+
+shaker()    
+
+def tryy():
+    asert()
+def asert ():
+    try:
+        assert square(2)==4
+    except AssertionError:
+        print("tttttt buggg")
+    try:
+        assert square(4)==16
+    except AssertionError:
+        print("bugggggg")    
+
+tryy()
+
+####################################################################
+
+
+
+def test():
+    str_test()
+def str_test():
+    try:
+        assert hello("shaker")=="hello shaker"  
+    except AssertionError:
+        print("erooooooorororo")
+#####################################################################
+test()        
+
+      
+def main():
+    name=input("what is your name ")
+    hello(name)
+    str_test()
+
+def hello(x):
+    print("hello",x)
+    return "hello,{x}  " 
+
+
+def str_test():
+        if hello("shaker")=="hello shaker":
+             print("pass")
+        else:
+             print("erooooooorororo")
+
+  
+        
+main()     
+ 
+name=input("what is your name ")
+print("hello",name)
+########################################################
+names=[]
+for i in range(3):
+    names.append(input("what is your name "))
+
+for name in sorted(names):    
+    print(f"hello,{name}")
+    
+
+
+
+
+open("na")
+
+names=input("your name ")
+file=open("names.txt","a")
+file.write(f"{names}\n")
+
+
+file=open("names.txt","r")
+for line in file:
+    print("hello",line.rstrip())
+file.close()    
+
+###################################################
+
+names=[]
+file=open("names.txt")
+for line in file:
+    names.append(line.rstrip())
+for name in sorted(names) :
+    print("hello",name)    
+file.close()    
+########################################################
+
+file=open("names.txt")
+for line in sorted (file):
+    print("hello",line.rstrip())
+file.close()    
+
+##########################################################
+
+file=open("names.txt")
+for line in file:
+    row=line.rstrip().split(",")
+    print(row[0],"is in ",row[1])
+file.close()        
+
+
+###################################################################
+import csv
+students=[]
+file=open("names.txt")
+for line in file:
+    name, city=line.rstrip().split(",")
+    student={"name":name,"city":city}
+    students.append(student)
+
+def get_city(student):
+    return student["city"]
+
+for student in sorted (students,key=get_city ):
+    print(student["name"] ,"is in " , student["city"])
+file.close()        
+
+########################################################
+
+import csv
+name=input("what is your name ")
+home=input("where is your home ")
+file=open("names.txt","a")
+writer=csv.writer(file)
+writer.writerow([name,home])
+file.close()
+
+#############################################
+
+email=input("what is your email ")
+if "@" in email and "." in email:
+    print("validate")
+else:
+    print("invalidate")    
+
+
+email=input("what is your email ")
+username,domain=email.split("@")
+if username and "."in domain:
+    print ("valid")
+else:
+    print ("invalid")    
+
+email=input("what is your email ")
+if "@"in email and "." in email :
+    print("valid")
+
+else:
+    print("invalid")    
+
+email=input("what is your email ")
+username,domain=email.split("@")
+if username and domain.endswith(".com"):
+    print ("valid")
+else:
+    print ("invalid")            
+
+import re
+email=input("what is your email ").strip()
+if re.search(r"^\w+@\w+..com$",email):
+    print("valid")
+else:
+    print("invalid")
+
+name = str(input("Enter Your Name : "))
+surname = str(input("Enter Your Surname : "))
+email = str(input("Enter Your Email : "))
+password = str(input("Enter Your Password : "))
+confPassword = str(input("Confirm Your Password : "))
+  
+if(password == confPassword):
+      print("User Accepted!")
+      user = createUser(name, surname, email, password)
+else:
+      print("User Rejected! Invalid Password Combination")
+
+
+name=input("enter your name ").strip()
+if ","in name:
+    first,last=name.split(",")
+    print("hello ",first+last)
+else:print("hello ",name)
+
+
+import re
+name=input("what is your name ")
+matches=re.search(r"^(.+),(.+)",name)
+if matches:
+    first,last=matches.groups()
+    print("hello",first,last)
+else:
+    print("hello",name)
+
+import re
+url=input("enter url ")
+username=re.sub(r"^(.+)(:|.)(.+)(.com/|.edu/|.aaa/)", "",url)
+print(username)
+
+
+
+def main():
+    student=get_student()
+    if student[0]=="ahmed":
+        student[1]="cairo"
+    print(f"{student[0]} from {student[1]}")
+
+def get_student():
+    name=input("name: ")
+    city=input("city: ")
+    return [name,city]
+main()
+
+class students:
+    def __init__(self,name,house) :
+        if not name:
+            raise ValueError("a7aa")
+        if house not in["cairo","riyadh"]:
+            raise ValueError("not in")
+        self.name=name
+        self.house=house
+
+def main():
+    student=get_student()
+    
+    print(f"{student.name} from {student.house}")
+
+def get_student():
+    
+    name=input("name: ")
+    house=input("house: ")
+    return students(name,house)
+    
+
+class Student:
+    def __init__(self, name, house):
+        if not name:
+            raise ValueError("Name cannot be empty")
+        if house not in ["cairo", "riyadh"]:
+            raise ValueError("House must be 'cairo' or 'riyadh'")
+        self.name = name
+        self.house = house
+
+    def __str__(self):
+        return "a student"
+      
+
+def main():
+    student = get_student()
+    print(student)
+    
+def get_student():
+    name = input("Enter the student's name: ")
+    house = input("Enter the student's house (cairo or riyadh): ")
+    return Student(name, house)
+
+if __name__ == "__main__":
+    main()
+   
+main()
+
+
+
+
+
+  
